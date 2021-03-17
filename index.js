b025eac47056e46873cacbd636a1eec1d1e2d5a0
ptrcko2 <61922326+ptrcko2@users.noreply.github.com>
2021-03-17 14:24:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ app.use((err, req, res, next) => {
 });
 
 async function login() {
-  await roblox.cookieLogin(config.user_cookie); // thanks for ruining logins roblox
+  await roblox.setCookie(config.user_cookie); // thanks for ruining logins roblox
   return await roblox.getCurrentUser();
 }
 
